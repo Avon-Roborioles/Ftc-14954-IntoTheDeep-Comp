df8f40a738fbb53d4cf2d225fa715328b736f309
TornadoBoah <nboekekitt@gmail.com>
2024-12-18 12:23:45 -0500

    Swing arm changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandGroups/Score.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandGroups/Score.java
@@ -7,19 +7,21 @@ import org.firstinspires.ftc.teamcode.commands.LiftCommands.LiftBottomCommand;
 import org.firstinspires.ftc.teamcode.commands.LiftCommands.LiftTopCommand;
 import org.firstinspires.ftc.teamcode.commands.SwingArmCommand.SwingArmDownCommand;
 import org.firstinspires.ftc.teamcode.commands.SwingArmCommand.SwingArmUpCommand;
+import org.firstinspires.ftc.teamcode.subsystems.BoxxySubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SwingArmSubsystem;
 
 public class Score extends SequentialCommandGroup {
     private SwingArmSubsystem swingArm;
     private LiftSubsystem lift;
+    private BoxxySubsystem box;
     public Score(SwingArmSubsystem swingArm, LiftSubsystem lift){
         addCommands(
                 new LiftTopCommand(lift),
-                new SwingArmUpCommand(swingArm),
+                new SwingArmUpCommand(swingArm, box),
                 new WaitCommand(1000),
                 new SwingArmDownCommand(swingArm),
-                new WaitCommand(1000),
+                new WaitCommand(500),
                 new LiftBottomCommand(lift)
         );
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/SwingArmCommand/SwingArmUpCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/SwingArmCommand/SwingArmUpCommand.java
@@ -2,13 +2,16 @@ package org.firstinspires.ftc.teamcode.commands.SwingArmCommand;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
+import org.firstinspires.ftc.teamcode.subsystems.BoxxySubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SwingArmSubsystem;
 
 public class SwingArmUpCommand extends CommandBase {
     private SwingArmSubsystem SwingArmSubsystem;
-    public SwingArmUpCommand(SwingArmSubsystem SwingArmSubsystem) {
+    private BoxxySubsystem box;
+    public SwingArmUpCommand(SwingArmSubsystem SwingArmSubsystem, BoxxySubsystem box) {
         this.SwingArmSubsystem = SwingArmSubsystem;
-        addRequirements(SwingArmSubsystem);
+        this.box = box;
+        addRequirements(SwingArmSubsystem, box);
     }
     @Override
     public void initialize() { SwingArmSubsystem.up(); }
